b56dadc1531849c70e2640426c974718e078159d
MaximVovk <maksemvovk@gmail.com>
2022-02-04 00:37:51 +0200

    add checkAuth for route "bank-list"

--- src/backend/server/api/bank-list.js
+++ src/backend/server/api/bank-list.js
@@ -1,9 +1,10 @@
 const { Router } = require('express')
 const getBanks = require('../../features/get-banks')
+const checkAuth = require('./middleware/check-auth')
 
 const router = module.exports = Router()
 
-router.post('/bank-list', async (req, res, next) => {
+router.post('/bank-list', checkAuth, async (req, res, next) => {
   try {
     const banks = await getBanks()
     res.send(banks)
