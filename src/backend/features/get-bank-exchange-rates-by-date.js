f2f73ea775d77bedf5874a8131743ef1ffae6115
MaximVovk <maksemvovk@gmail.com>
2022-02-04 00:35:56 +0200

    add validation for endpoint "exchange-rates-by-date"

--- src/backend/features/get-bank-exchange-rates-by-date.js
+++ src/backend/features/get-bank-exchange-rates-by-date.js
@@ -1,11 +1,23 @@
+const Joi = require('joi')
 const pg = require('../infra/pg')
 
-module.exports = function (params) {
+const schema = Joi.object({
+  date: Joi.date().iso().required(),
+  bank_id: Joi.number().integer().required()
+})
+
+module.exports = function (params, res) {
+  const { error } = schema.validate(params)
+
+  if (error) {
+    res.status(400).send({ message: 'Invalid data', errors: error.details })
+  }
+
   return pg('exchange_currencies as e')
     .select(
       'e.bay',
       'e.sale',
-      pg.raw("e.date::text"),
+      pg.raw('e.date::text'),
       'b.name as bank_name',
       'b.id as bank_id',
       'c.name as currency'
