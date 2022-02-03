b91cd789a0987121c29a2fba664181047f30bba0
MaximVovk <maksemvovk@gmail.com>
2022-02-04 00:38:18 +0200

    add checkAuth for route "exchange-rates-by-range"

--- src/backend/server/api/exchange-rates-by-range.js
+++ src/backend/server/api/exchange-rates-by-range.js
@@ -1,12 +1,14 @@
 const { Router } = require('express')
+const checkAuth = require('./middleware/check-auth')
+
 const getExchangeRatesByRange = require('../../features/get-exchange-rates-by-range')
 
 const router = module.exports = Router()
 
-router.post('/exchange-rates-by-range', async (req, res, next) => {
+router.post('/exchange-rates-by-range', checkAuth, async (req, res, next) => {
   try {
     const body = req.body
-    const result = await getExchangeRatesByRange(body)
+    const result = await getExchangeRatesByRange(body, res)
     res.send(result)
   } catch (error) {
     next(error)
