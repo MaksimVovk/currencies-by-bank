fb7ccc9552d2814599554756fd42526193d1ca72
MaximVovk <maksemvovk@gmail.com>
2022-02-06 00:27:01 +0200

    fix pg config

--- src/infra/pg.js
+++ src/infra/pg.js
@@ -1,3 +1,3 @@
 const environment = process.env.NODE_ENV || 'production'
-const config = require('./etc/postgres/knexfile')[environment];
+const config = require('../../etc/postgres/knexfile')[environment];
 module.exports = require('knex')(config);
